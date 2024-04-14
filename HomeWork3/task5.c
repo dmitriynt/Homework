0d3ea3b6080e066e66e8e435904b991e23779fdc
Dmitriy Kiverin <dmitriynt@gmail.com>
2024-04-14 21:11:51 +0500

    Edited task5

--- HomeWork3/task5.c
+++ HomeWork3/task5.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-#include <locale.h>
 #include <math.h>
 
 int main(){
     char str[3];
-    int sum = 0;
+    int res = 1;
     scanf ("%s", str);
     for (int i=0; i < 3; ++i) {
         char c = str[i];
-        sum += c - '0';
+        res *= c - '0';
     }
-    printf("%d", sum);
-    scanf ("%s", str);
+    printf("%d", res);
     return 0;
 }
